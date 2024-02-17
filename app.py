eeb53f048b4a647d88c6febc2e48218af0384f89
Shivank Kapur <shivankkapur2004@gmail.com>
2024-02-18 02:41:00 +0530

    wrote the frontend using streamlit

--- app.py
+++ app.py
@@ -6,12 +6,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-st.title("Code Review App")
+st.title("Code Review App ☕️")
 st.header("Please upload your .py file here")
 
 data = st.file_uploader("Upload python file", type=".py")
 
 if data:
+    # decoded content of 'data'
     stringio = StringIO(data.getvalue().decode("utf-8"))
+    # reading the content of stringio object and storing it in the variable
     fetched_data = stringio.read()
     st.write(fetched_data)
